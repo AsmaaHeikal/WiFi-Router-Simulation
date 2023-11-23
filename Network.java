c092db02dcf8ca01777ad39af1ed22d4e3322087
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-11-23 18:40:55 +0200

    project main classes

---
+++ Network.java
@@ -0,0 +1,25 @@
+public class Network {
+    //This class contains the main method in which the user is asked
+    //for two inputs:
+    //● N: max number of connections a router can accept
+    //● TC: total number of devices that wish to connect.
+    //● TC lines that contain: Name of each device, and its Type.
+    public static void main(String[] args) {
+    }
+}
+
+class Device{
+//Represent different devices (threads) that can be connected to the
+//router;
+//Each device has its own name (i.e. C1) and type (i.e. mobile, pc, tablet...) and it may
+//perform three activities: connect, perform online activity and disconnect/logout.
+}
+
+class Router{
+//This class contains a list of connections and methods to occupy a
+//connection and release a connection.
+}
+
+class Semaphore{
+//As given in the synchronization lab (Check Lab 3).
+}
