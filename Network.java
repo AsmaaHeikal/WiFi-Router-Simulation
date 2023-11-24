8e1d8c43b92e213bb7a61d40e169742fb9b19eea
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-11-24 13:52:22 +0200

    update

--- Network.java
+++ Network.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,18 +9,18 @@ class Semaphore {
         value = initial;
     }
 
-    public synchronized void acquire() {
+    public synchronized void acquire(String name,String type) {
         value--;
 
         if (value < 0) {
             try {
-                System.out.println("Arrive and Waiting");
+                System.out.println("("+ name+") ("+type+") "+"Arrived and Waiting");
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         } else {
-            System.out.println("Arrived");
+            System.out.println("("+ name+") ("+type+") "+"Arrived");
         }
     }
 
@@ -33,25 +34,30 @@ class Semaphore {
 }
 
 class Router {
-    private final Semaphore semaphor;
+    private final Semaphore semaphore;
     int numberOfConnection;
     int maxConnections;
+    ArrayList<Device> devices = new ArrayList<>();
 
-    public Router(int maxConnections) {
+    public Router(int maxConnections, ArrayList<Device> devices) {
         this.maxConnections = maxConnections;
-        semaphor = new Semaphore(maxConnections);
+        semaphore = new Semaphore(maxConnections);
         numberOfConnection = 0;
+        this.devices = devices;
     }
-    public void occupyConnection(String deviceName) throws InterruptedException{
-        numberOfConnection++;
-        semaphor.acquire();
-//        //Connection 1: C1 Occupied
-        System.out.println("Connection "+ numberOfConnection+" : " + deviceName + " Occupied");
+    public void occupyConnection(String deviceName,String deviceType) throws InterruptedException{
+        semaphore.acquire(deviceName,deviceType);
+        synchronized (this) {
+            numberOfConnection++;
+            System.out.println("Connection " + numberOfConnection + " : " + deviceName + " Occupied");
+        }
     }
     public void releaseConnection(String deviceName) throws InterruptedException{
-        numberOfConnection--;
-        semaphor.release();
-        System.out.println("Connection "+ numberOfConnection+" : " + deviceName + " Logged out");
+        semaphore.release();
+        synchronized (this) {
+            System.out.println("Connection " + numberOfConnection + " : " + deviceName + " Logged out");
+            numberOfConnection--;
+        }
     }
     public int getNumberOfConnection() {
         return numberOfConnection;
@@ -80,7 +86,8 @@ class Device extends Thread {
     @Override
     public void run() {
         try {
-            router.occupyConnection(name);
+            //make occupyConnection called once the deviced started
+//            router.occupyConnection(name,type);
             System.out.println("Connection " + router.getNumberOfConnection() + " : " + name + " Logged in\n"
                     +"Connection " + router.getNumberOfConnection() + " : " + name + " Performs online activity");
             sleepRandomTime();
@@ -99,18 +106,19 @@ public class Network {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is the number of WI-FI Connections?");
         int N = scanner.nextInt();
-        Router router = new Router(N);
         System.out.println("What is the number of devices Clients want to connect?");
         int TC = scanner.nextInt();
         Queue<Device> queue = new LinkedList<>();
-        Device [] devices = new Device[TC];
+        ArrayList<Device> devices = new ArrayList<>();
+        Router router = new Router(N,devices);
         for (int i = 0; i < TC; i++) {
             String name = scanner.next();
             String type = scanner.next();
-            devices[i] = new Device(router,name, type);
+            devices.add(new Device(router,name, type));
         }
-        for(int i=0;i<N;i++){
-            devices[i].start();
+        for(int i=0;i<TC;i++){
+            router.occupyConnection(devices.get(i).name(),devices.get(i).getType());
+            devices.get(i).start();
         }
     }
 }
